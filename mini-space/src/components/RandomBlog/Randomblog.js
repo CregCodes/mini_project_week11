53d6bbf3986dbf704dfd93e84531748d224a6c42
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2022-12-04 14:06:38 +0000

    Added ReadMe documentation & JSDocs/ comments in App.css file

--- mini-space/src/components/RandomBlog/Randomblog.js
+++ mini-space/src/components/RandomBlog/Randomblog.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 
+/** RandomBlog
+ * Takes in a state & prop (to be developed)
+ * Will render random "log entries" for a user to read through
+ * Will be linked to a button to trigger render
+ * For research- aim to have a dynamic render animation when button event is triggered
+ */
+
 export default class Randomblog extends Component {
   render() {
     return (
